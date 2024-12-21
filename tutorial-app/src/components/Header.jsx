e1fe96f03442bd95c0ee2783f6d7bfc3e04d2313
Bouchaker ilyes <105610322+ilsbcr@users.noreply.github.com>
2024-12-21 14:24:25 +0100

    start challenge1

---
+++ tutorial-app/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.css";
+
+export default function Header() {
+
+    return (
+        <header className="header header-layout">
+            <h1>Welcom to this site</h1>
+        </header>
+    );
+}
